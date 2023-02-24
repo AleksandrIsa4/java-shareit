6dae85eb452a324d394a81a5b84d54187e7b1a32
AleksandrIsa4 <isachenkov.am@yandex.ru>
2023-02-24 21:51:51 +0300

    feat: Создана база данных приложения ShareIt. Версия 1.04

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,18 +1,10 @@
 package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@DataJpaTest
-@ExtendWith(SpringExtension.class)
-@PropertySource("classpath:application.properties")
-@EntityScan(basePackages = "ru.practicum.shareit")
-@EnableJpaRepositories(basePackages = "ru.practicum.shareit")
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class ShareItTests {
 
     @Test
