eb743e41c7a1589947e440db6232bc9dc937098b
AleksandrIsa4 <isachenkov.am@yandex.ru>
2023-02-24 22:14:53 +0300

    feat: Создана база данных приложения ShareIt. Версия 1.06

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -3,15 +3,9 @@ package ru.practicum.shareit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.practicum.shareit.config.DbConfig;
 
 @AutoConfigureDataJpa
-@SpringBootTest(classes = DbConfig.class,
-        properties = {
-                "spring.jpa.properties.hibernate.format_sql=true",
-                "spring.jpa.show-sql=true",
-                "spring.main.allow-circular-references=true"
-        })
+@SpringBootTest
 class ShareItTests {
 
     @Test
